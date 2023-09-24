dd9fe325b956fa78e6f0e39a08f897b4bc954de9
Perkles <joaomatheus696@outlook.com>
2023-09-24 14:53:44 -0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Models/Iphone.cs 	modified:   Models/Nokia.cs 	modified:   Models/Smartphone.cs 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("/n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "4887", modelo: "Modelo 2", imei: "2222222", memoria : 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
